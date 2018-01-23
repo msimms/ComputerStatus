64f4e5dbae8cc7884a6910ed16d2c5e51667c78f
Mike Simms <msimms01@gmail.com>
2018-01-22 19:41:49 -0500

    Added setup.py script.

---
+++ client/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import platform
+
+target = platform.system()
+requirements = ['psutil', 'requests']
+if target == 'Windows':
+    requirements.append('pypiwin32')
+    requirements.append('wmi')
+
+setup(
+    name='computerstatus',
+    version='0.1.0',
+    description='A Python project for monitoring the health of a computer',
+    url='https://github.com/msimms/ComputerStatus',
+    author='Mike Simms',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=requirements,
+)
