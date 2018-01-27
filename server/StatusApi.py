6d7d09cfacce720b75dce880bdab0c23c2263d8f
Mike Simms <msimms01@gmail.com>
2018-01-27 08:45:20 -0500

    Can now request multiple graphs at once.

--- server/StatusApi.py
+++ server/StatusApi.py
@@ -20,6 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import json
 import StatusDb
 
 class StatusApi(object):
@@ -27,38 +28,37 @@ class StatusApi(object):
         super(StatusApi, self).__init__()
         self.database = StatusDb.MongoDatabase(root_dir)
 
-    def format_graph_point(self, datetime_str, value):
-        graph_str = "\t\t\t\t{ date: new Date(" + datetime_str + "), value: " + str(value) + " },\n"
-        return graph_str
+    def handle_graph_data_request(self, device_id, params, start_time):
+        graph_data = []
 
-    def append_graph_point(self, datetime_str, status, key):
-        if key in status:
-            return self.format_graph_point(datetime_str, status[key])
-        else:
-            return self.format_graph_point(datetime_str, 0)
-
-    def handle_graph_data_request(self, device_id, param, start_time):
-        graph_str = "["
         statuses = self.database.retrieve_status(device_id)
         if statuses is not None:
             for status in statuses:
                 if "datetime" in status:
                     datetime_num = int(status["datetime"]) * 1000
                     if datetime_num > start_time:
-                        datetime_str = str(datetime_num)
-                        graph_str += self.append_graph_point(datetime_str, status, param)
-        graph_str += "]"
+                        point_data = {}
+                        point_data['datetime'] = str(datetime_num)
+                        for param in params:
+                            if param in status:
+                                point_data[param] = status[param]
+                            else:
+                                point_data[param] = 0
+                        graph_data.append(point_data)
+
+        graph_str = "{\"points\":" + json.dumps(graph_data) + "}"
         return graph_str
 
     def handle_api_1_0_request(self, args, values):
         if len(args) > 0:
             request = args[0]
+            
             if request == 'upload':
                 if "device_id" in values and "datetime" in values:
                     self.database.create_status(values)
                     return True, ""
             elif request == 'graph_data':
-                if "device_id" in values and "param" in values and "start_time" in values:
-                    response = self.handle_graph_data_request(values["device_id"], values["param"], int(values["start_time"]))
+                if "device_id" in values and "params" in values and "start_time" in values:
+                    response = self.handle_graph_data_request(values["device_id"], values["params"].split(','), int(values["start_time"]))
                     return True, response
         return False, ""
