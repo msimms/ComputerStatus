2bf0eb7e158a36243f594b34e57884bc794c8b6e
Mike Simms <msimms01@gmail.com>
2018-04-18 14:37:04 -0400

    Added the ability to run user provided code after each iteration of the client.

---
+++ client/keys.py
@@ -0,0 +1,34 @@
+# MIT License
+# 
+# Copyright (c) 2018 Mike Simms
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+KEY_DEVICE_ID = 'device_id'
+KEY_DATETIME = 'datetime'
+KEY_GPU_NAME = 'gpu - name'
+KEY_GPU_TEMPERATURE = 'gpu - temperature'
+KEY_GPU_PERCENT = 'gpu - percent'
+KEY_CPU_PERCENT = 'cpu - percent'
+KEY_CPU_USER_TIMES = 'cpu - user times'
+KEY_CPU_TEMPERATURE = 'cpu - temperature'
+KEY_VIRTUAL_MEM_TOTAL = 'virtual memory - total'
+KEY_VIRTUAL_MEM_PERCENT = 'virtual memory - percent'
+KEY_NETWORK_BYTES_SENT = 'network - bytes sent'
+KEY_NETWORK_BYTES_RECEIVED = 'network - bytes received'
--- client/monitor.py
+++ client/monitor.py
@@ -21,19 +21,20 @@
 # SOFTWARE.
 
 import argparse
-import datetime
+import imp
 import logging
 import os
-import requests
 import signal
 import subprocess
 import sys
 import threading
 import time
-import psutil
 import urlparse
 import uuid
+import requests
+import psutil
 import cpu_status
+import keys
 
 g_monitor_thread = None
 
@@ -45,11 +46,12 @@ def signal_handler(signal, frame):
         g_monitor_thread.terminate()
 
 class MonitorThread(threading.Thread):
-    def __init__(self, interval, server, verbose, do_cpu_check, do_mem_check, do_net_check, do_gpu_check):
+    def __init__(self, interval, server, post_file, verbose, do_cpu_check, do_mem_check, do_net_check, do_gpu_check):
         threading.Thread.__init__(self)
         self.stopped = threading.Event()
         self.interval = interval
         self.server = server
+        self.post_file = post_file
         self.verbose = verbose
         self.do_cpu_check = do_cpu_check
         self.do_mem_check = do_mem_check
@@ -75,8 +77,8 @@ class MonitorThread(threading.Thread):
     def send_to_server(self, values):
         """Sends the values to the server for archival."""
         try:
-            values['device_id'] = self.device_id
-            values['datetime'] = str(int(time.time()))
+            values[keys.KEY_DEVICE_ID] = self.device_id
+            values[keys.KEY_DATETIME] = str(int(time.time()))
             url = self.server + "/api/1.0/upload"
             r = requests.post(url, data=values)
             logging.info("Server Response: " + str(r))
@@ -89,12 +91,12 @@ class MonitorThread(threading.Thread):
         """Appends GPU values to the 'values' dictionary."""
         try:
             process = subprocess.Popen(['nvidia-smi', '--query-gpu=name,pci.bus_id,driver_version,pstate,pcie.link.gen.max,pcie.link.gen.current,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used', '--format=csv'], stdout=subprocess.PIPE)
-            out_str, err_str = process.communicate()
+            out_str, _ = process.communicate()
             out_str = out_str.split('\n')[1]
             out = out_str.split(',')
-            values['gpu - name'] = out[0].strip(' \t\n\r')
-            values['gpu - temperature'] = int(out[6].strip(' \t\n\r'))
-            values['gpu - percent'] = int(out[7].strip(' \t\n\r%%s'))
+            values[keys.KEY_GPU_NAME] = out[0].strip(' \t\n\r')
+            values[keys.KEY_GPU_TEMPERATURE] = int(out[6].strip(' \t\n\r'))
+            values[keys.KEY_GPU_PERCENT] = int(out[7].strip(' \t\n\r%%s'))
         except:
             logging.error("Error collecting GPU stats.")
 
@@ -102,9 +104,9 @@ class MonitorThread(threading.Thread):
         """Appends current CPU values to the 'values' dictionary."""
         try:
             cpu_percent = psutil.cpu_percent()
-            values['cpu - percent'] = cpu_percent
+            values[keys.KEY_CPU_PERCENT] = cpu_percent
             cpu_times = psutil.cpu_times()
-            values['cpu - user times'] = cpu_times.user
+            values[keys.KEY_CPU_USER_TIMES] = cpu_times.user
         except:
             logging.error("Error collecting CPU stats.")
 
@@ -119,8 +121,8 @@ class MonitorThread(threading.Thread):
         """Appends current memory values to the 'values' dictionary."""
         try:
             virt_mem = psutil.virtual_memory()
-            values['virtual memory - total'] = virt_mem.total
-            values['virtual memory - percent'] = virt_mem.percent
+            values[keys.KEY_VIRTUAL_MEM_TOTAL] = virt_mem.total
+            values[keys.KEY_VIRTUAL_MEM_PERCENT] = virt_mem.percent
         except:
             logging.error("Error collecting memory stats.")
 
@@ -128,8 +130,17 @@ class MonitorThread(threading.Thread):
         """Appends current network stats to the 'values' dictionary."""
         try:
             net_io = psutil.net_io_counters()
-            values['network - bytes sent'] = net_io.bytes_sent
-            values['network - bytes received'] = net_io.bytes_recv
+            values[keys.KEY_NETWORK_BYTES_SENT] = net_io.bytes_sent
+            values[keys.KEY_NETWORK_BYTES_RECEIVED] = net_io.bytes_recv
+        except:
+            logging.error("Error collecting network stats.")
+
+    def execute_post_file(self, values):
+        """Executes the post process file. This is where the user can specify logic to run after each check."""
+        try:
+            if os.path.isfile(self.post_file):
+                loaded_module = imp.load_source("", self.post_file)
+                loaded_module.do(values)
         except:
             logging.error("Error collecting network stats.")
 
@@ -153,13 +164,18 @@ class MonitorThread(threading.Thread):
             if self.verbose:
                 print values
 
-if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal_handler)
+            if self.post_file is not None and len(self.post_file) > 0:
+                self.execute_post_file(values)
+
+def main():
+    """Entry point"""
+    global g_monitor_thread
 
     # Parse command line options.
     parser = argparse.ArgumentParser()
     parser.add_argument("--interval", type=int, default=60, help="Frequency (in seconds) at which to sample.", required=False)
     parser.add_argument("--server", type=str, action="store", default="", help="Remote logging server (optional)", required=False)
+    parser.add_argument("--post", type=str, action="store", default="", help="Post processing code module (optional)", required=False)
     parser.add_argument("--verbose", action="store_true", default=True, help="TRUE to enable verbose mode", required=False)
     parser.add_argument("--log", action="store", default="", help="Name of the log file (optional)", required=False)
     parser.add_argument("--cpu", action="store_true", default=True, help="TRUE if sampling the CPU", required=False)
@@ -185,10 +201,15 @@ if __name__ == "__main__":
         logging.basicConfig(filename=args.log,level=logging.DEBUG)
 
     # Start the monitor thread.
-    g_monitor_thread = MonitorThread(args.interval, server, args.verbose, args.cpu, args.mem, args.net, args.gpu)
+    g_monitor_thread = MonitorThread(args.interval, server, args.post, args.verbose, args.cpu, args.mem, args.net, args.gpu)
     g_monitor_thread.start()
 
     # Wait for it to finish. We do it like this so that the main thread isn't blocked and can execute the signal handler.
     while g_monitor_thread.isAlive():
         time.sleep(1)
     g_monitor_thread.join()
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    main()
