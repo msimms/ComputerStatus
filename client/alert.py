4969cddaa062299d53a16bcf927d82c0a7fa46ce
Mike Simms <msimms01@gmail.com>
2018-04-17 23:53:25 -0400

    Removed unnecessary code from alert.py.

--- client/alert.py
+++ client/alert.py
@@ -20,57 +20,18 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import argparse
-import signal
 import smtplib
-import sys
-import threading
-import time
-
-g_alert_thread = None
-
-def signal_handler(signal, frame):
-    global g_alert_thread
-
-    print "Exiting..."
-    if g_alert_thread:
-        g_alert_thread.terminate()
 
 def send_sms(to, msg):
+    """Sends an SMS message. Not implemented yet."""
     pass
 
 def send_email(from_addr, to_addr, msg, username, password, server):
-	msg = "To:" + to_addr + "\r\nFrom: " + from_addr + "\r\nSubject:New IP Address\r\n\r\n" + msg + "\r\n\r\n"
-	
-	server = smtplib.SMTP(server + ':587')
-	server.starttls()
-	server.login(username, password)
-	server.sendmail(from_addr, to_addr, msg)
-	server.quit()
+    """Connects to an SMTP server using TLS to send an email."""
+    msg = "To:" + to_addr + "\r\nFrom: " + from_addr + "\r\nSubject:New IP Address\r\n\r\n" + msg + "\r\n\r\n"
 
-class AlertThread(threading.Thread):
-    def __init__(self, server):
-        threading.Thread.__init__(self)
-        self.stopped = threading.Event()
-
-if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal_handler)
-
-    # Parse command line options.
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--server", type=str, action="store", default="", help="Remote logging server (optional)", required=False)
-
-    try:
-        args = parser.parse_args()
-    except IOError as e:
-        parser.error(e)
-        sys.exit(1)
-
-    # Start the monitor thread.
-    g_alert_thread = AlertThread(args.server)
-    g_alert_thread.start()
-
-    # Wait for it to finish. We do it like this so that the main thread isn't blocked and can execute the signal handler.
-    while g_alert_thread.isAlive():
-        time.sleep(1)
-    g_alert_thread.join()
+    server = smtplib.SMTP(server + ':587')
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(from_addr, to_addr, msg)
+    server.quit()
