414c94bf55e269c052b5701877e5bccb007a3451
Mike Simms <msimms01@gmail.com>
2018-10-29 20:38:06 -0500

    Added setup.py and requirements.txt for the server component.

---
+++ server/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+requirements = ['cherrypy', 'mako', 'bson', 'pymongo', 'bcrypt', 'markdown']
+
+setup(
+    name='computerstatus',
+    version='0.8.0',
+    description='',
+    url='https://github.com/msimms/ComputerStatus',
+    author='Mike Simms',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=requirements,
+)
